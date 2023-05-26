028efb6c043405e9e0409416c4f1bbaeec6ec25f
amandaanaju23 <125404603+amandaanaju23@users.noreply.github.com>
2023-05-26 15:36:05 -0300

    Add files via upload

---
+++ main.kt
@@ -0,0 +1,21 @@
+fun main() {
+  println("Olá Mundo")
+  var nome:String = "Amanda"
+  val nascimento:Int = 2005
+
+println(nome)          // exibe o nome
+println(nascimento)     // exibe o nascimento 
+
+  val myNum: Int = 5                // Int
+  val myDoubleNum: Double = 5.99    // Double
+  val myLetter: Char = 'A'          // Char
+  val myBoolean: Boolean = true     // Boolean
+  val myText: String = "Eae"        // String
+
+  println (myNum)
+  println (myDoubleNum)
+  println (myLetter)
+  println (myBoolean)
+  println (myText)
+  
+}
